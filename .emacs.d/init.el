a48beb90eb161d5ff20b9fc997ea0e53aeab955d
Lasse Maatta <lasse.olavi.maatta@gmail.com>
2021-07-14 12:57:35 +0300

    Update emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,26 +1,6 @@
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t)
- '(package-selected-packages
-   '(diff-hl git-gutter dired-sidebar use-package clojure-mode lsp-mode cider lsp-treemacs flycheck company)))
+ '(inhibit-startup-screen t))
 (custom-set-faces)
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- 
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- 
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
