57406e97593df6340c304854856d1bfc58faa411
Lasse Määttä <lasse.olavi.maatta@gmail.com>
2023-02-18 18:51:03 +0200

    Fetch package list automatically at startup but in the background

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -28,9 +28,11 @@
 (unless package--initialized
   (package-initialize))
 
-;; Download the package list if we've yet to do so
-(when (not package-archive-contents)
-  (package-refresh-contents))
+;; Download the package list immediately if we've yet to do so (e.g. after reinstalling)
+;; Otherwise fetch in the background
+(if (not package-archive-contents)
+  (package-refresh-contents)
+  (package-refresh-contents t))
 
 (let ((elapsed (float-time (time-subtract (current-time)
                                           emacs-start-time))))
