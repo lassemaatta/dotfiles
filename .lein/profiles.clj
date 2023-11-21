7d5ac21f7777ef9815ea77499821367e7612df03
Lasse Määttä <lasse.olavi.maatta@gmail.com>
2023-11-21 15:06:41 +0200

    Add global lein profiles

---
+++ .lein/profiles.clj
@@ -0,0 +1,17 @@
+{:user           {:plugins      [[cider/cider-nrepl "LATEST"]]
+                  :dependencies [[nrepl/nrepl "LATEST"]]
+                  :test-refresh {:quiet        true
+                                 :changes-only true}}
+
+ :root-reveal    {:dependencies [[vlaaad/reveal "1.3.274"]]
+                  :repl-options {:nrepl-middleware [vlaaad.reveal.nrepl/middleware]}
+                  :jvm-opts     ["-Dvlaaad.reveal.prefs={:font-size,16}"]}
+
+ :root-benchmark {:dependencies [[criterium "0.4.6"]
+                                 [com.clojure-goes-fast/clj-async-profiler "1.0.3"]]
+                  :jvm-opts     ["-Djdk.attach.allowAttachSelf"]}
+
+ :root-local-dev {:jvm-opts ["-XX:-OmitStackTraceInFastThrow"
+                             "-Dclojure.main.report=stderr"]}
+
+ :root-kondo     {:dependencies [[clj-kondo/clj-kondo "LATEST"]]}}
