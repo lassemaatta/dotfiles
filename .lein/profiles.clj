0255c5b9ecd9e0e58555c70d74deefcc5ad130ce
Lasse Määttä <lasse.maatta@cloudpermit.com>
2024-03-30 15:02:13 +0200

    Don't print links to changelogs when running "lein outdated"

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user           {:plugins      [[cider/cider-nrepl "LATEST"]]
-                  :aliases      {"outdated" ["with-profile" "root-antq" "run" "-m" "antq.core" "--skip=pom"]}
+                  :aliases      {"outdated" ["with-profile" "root-antq" "run" "-m" "antq.core" "--skip=pom" "--no-changes"]}
                   :test-refresh {:quiet        true
                                  :changes-only true}}
  :root-antq      {:dependencies [[com.github.liquidz/antq "RELEASE"]
